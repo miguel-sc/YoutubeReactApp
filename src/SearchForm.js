2381192a1bca3ee9a4a033ad99f277bf983637a8
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2017-12-01 22:01:31 +0100

    added SearchForm.js

---
+++ src/SearchForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SearchForm extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search" />
+  			<input type="submit" />
+			</form>
+    );
+  }
+}
+
+export default SearchForm;
