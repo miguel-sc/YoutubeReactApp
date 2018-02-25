85c919d320276bb6ff24b73aeef49e0bd5532966
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2018-02-25 18:12:38 +0100

    HomePage now showing top videos

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { setSearchTerm } from './../actions'
+import { fetchSearchResults, fetchSearchResultsStats, selectVideo, setSearchTerm, selectVideoStats } from './../actions'
 import SearchForm from './../components/SearchForm'
+import VideoList from './../components/VideoList'
 
 const mapStateToProps = (state) => {
-  return {}
+  return {
+    searchResults: state.searchResults,
+    searchResultsStats: state.searchResultsStats
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
+    fetchSearchResults: fetchSearchResults,
+    fetchSearchResultsStats: fetchSearchResultsStats,
+    selectVideo: selectVideo,
+    selectVideoStats: selectVideoStats,
     setSearchTerm: setSearchTerm
   }, dispatch)
 }
@@ -20,6 +28,14 @@ class HomePage extends Component {
       <div>
         <SearchForm
           setSearchTerm = { this.props.setSearchTerm }
+        />
+        <VideoList
+          videoList = { this.props.searchResults }
+          videoListStats = { this.props.searchResultsStats }
+          selectVideo = { this.props.selectVideo }
+          fetchVideoList = { (pageToken) => this.props.fetchSearchResults('', pageToken) }
+          fetchVideoListStats = { this.props.fetchSearchResultsStats }
+          selectVideoStats = { this.props.selectVideoStats }
         />
       </div>
     )
